b94abc6073b312d20d337cbcef7ecc83aa59a098
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-06-27 22:56:54 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery');
+
+function createMarckup(arr) {
+  const marckUp = arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item" >
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join(' ');
+  return marckUp;
+}
+ulGallery.insertAdjacentHTML('beforeend', createMarckup(galleryItems));
+// console.log(createMarckup(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
