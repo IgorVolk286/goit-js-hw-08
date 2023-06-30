7cc0372219b79268dec9d533169da22308c19f00
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-06-30 10:16:24 +0300

    3 task end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import { values } from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', formSubmit);
+
+form.addEventListener('input', throttle(onInputClick, 500));
+
+const KEY_FORM_LS = 'feedback-form-state';
+
+const inputData = JSON.parse(localStorage.getItem(KEY_FORM_LS)) ?? {};
+
+checkInputAfterReload();
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+
+  console.log(JSON.parse(localStorage.getItem(KEY_FORM_LS)));
+
+  localStorage.removeItem(KEY_FORM_LS);
+}
+
+function onInputClick(event) {
+  inputData[event.target.name] = event.target.value;
+
+  localStorage.setItem(KEY_FORM_LS, JSON.stringify(inputData));
+}
+// console.log(inputData);
+
+function checkInputAfterReload() {
+  if (inputData) {
+    const { email, message } = form.elements;
+    email.value = inputData.email || '';
+    message.value = inputData.message || '';
+  }
+}
